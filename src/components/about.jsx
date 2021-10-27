d237fe6fb0b7d20be31602965d94fda3e9c85555
Abhinish Sharma <44517818+Abhinish098@users.noreply.github.com>
2021-10-27 13:26:08 +0530

    Minor changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,5 @@
 
-import pic13 from '../Images/13.jpg';
+import pic13 from '../Images/14-1.jpeg';
 export const About = (props) => {
   return (
     <div id='about'>
@@ -12,10 +12,9 @@ export const About = (props) => {
           <div className='col-xs-12 col-sm-12 col-md-12 col-lg-6'>
             <div className='about-text' >
               <h2 style={{ marginTop: "40px" }}>About Us</h2>
-              <p style={{ textAlign: 'left' }}>Dr.Manish kumar Sharma  is currently working as Senior resident in Dept. of Orthopaedics at ESIC Model Hospital, Rourkela.
+              <p style={{ textAlign: 'left' }}>Dr.Manish kumar Sharma  is currently working as Senior resident in Dept. of Orthopaedics at Samaleswari Hospital, Sundargadh.
                 Completed his MBBS from Vydehi Medical College, Bangalore(Karnataka) and went for his PG training in Orthopedics from Hi-Tech Medical College, Bhubaneshwar (Odisha) in 2017.
-                He has about 1 year 4 months exprience as senior residentin the Dept. of ORTHOPEDICS in Hi-tech medical college and Hospital, Rourkela.
-                He has 4 months exprience as a senior resident in the Dept.of Orthopaedics at Care Hospital, BBSR.Compassionate doctor who always considers my patients’ needs and comfort levels during diagnosis and treatment, and works to ensure they have the best possible experience.</p>
+                He has over 3 year exprience as senior residentin the Dept. of ORTHOPEDICS. A Compassionate doctor who always considers my patients’ needs and comfort levels during diagnosis and treatment, and works to ensure they have the best possible experience.</p>
             </div>
           </div>
         </div>
