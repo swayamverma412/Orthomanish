18424bd7a1743525b93e999eb0353a488e0d565e
Abhinish Sharma <44517818+Abhinish098@users.noreply.github.com>
2021-11-03 18:31:01 +0530

    Firebase Analytics enabled

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBLyYU8DjQiPgybUR4X2_Zo9X-zNSDS9Ek",
-  authDomain: "orthomanish-f6e8a.firebaseapp.com",
-  projectId: "orthomanish-f6e8a",
-  storageBucket: "orthomanish-f6e8a.appspot.com",
-  messagingSenderId: "591658081746",
-  appId: "1:591658081746:web:77cd6c2128adcf61b1bc0b",
-  measurementId: "G-Z7XB5G508S",
+  apiKey: "AIzaSyCU7qc2jDOpcvWTalXWLiypyJIySqyl3kg",
+  authDomain: "orthomanish-90ed8.firebaseapp.com",
+  projectId: "orthomanish-90ed8",
+  storageBucket: "orthomanish-90ed8.appspot.com",
+  messagingSenderId: "423275903748",
+  appId: "1:423275903748:web:187efd98bb8d3324bdae51",
+  measurementId: "G-7SCGHLQ202"
 });
 
 var db = firebase.firestore();
